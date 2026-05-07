99b7ac3556d15976986c15f59a7237b4bb18d559
pathanbashakhan <pathanbasha619@gmail.com>
2026-05-07 21:42:13 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    // The main method is the starting point of any Java program
+    public static void main(String[] args) {
+        // This line prints text to the console
+        System.out.println("Hello, World!");
+    }
+}
